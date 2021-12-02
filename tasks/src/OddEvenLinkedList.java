d4bf98c027010c21cf3b9affcb6df3b2914e7a90
Artem Kulin <kulinartem@gmail.com>
2021-12-02 12:45:46 +0300

    Add check NPE for task OddEvenLinkedList

--- tasks/src/OddEvenLinkedList.java
+++ tasks/src/OddEvenLinkedList.java
@@ -28,17 +28,26 @@ public class OddEvenLinkedList {
 
 
     public static ListNode oddEvenList(ListNode head) {
-        ListNode even = head.next;
-        ListNode odd = head;
-        ListNode evenHead = even;
+        if (head == null) return null;
 
+        ListNode even = head.next; // this pointer start from index 2(1 based indexing
+        ListNode odd = head; // this pointer start from index 1
+        ListNode evenHead = even; // this pointer needed to concat odd and even linked list
+
+        // how do we decide our loop condition
+        // the pointer on index 2 will decide loop condition  as it is 1 step ahead odd pointer
         while(even!=null && even.next!=null){
+            // as each odd or even element present at gap of 2 step
+            // put 2 step ahead element pointer into current odd index element
             odd.next = odd.next.next;
+            // put 2 step ahead element pointer into current even index element
             even.next = even.next.next;
+            // after updating move odd and even, to new element pointer inserted into its next
             odd = odd.next;
             even = even.next;
         }
 
+        // concat odd number linked tail with , head of even number linked list
         odd.next = evenHead;
 
         return head;
