212ffe180aa415fd8765d7a6b34483457818dc93
Artem Kulin <kulinartem@gmail.com>
2022-12-10 15:28:10 +0300

    Add solution for maximum product of splitted binary tree

---
+++ tasks/src/maximum_product_of_splitted_binary_tree/Solution.java
@@ -0,0 +1,29 @@
+package maximum_product_of_splitted_binary_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    static int MOD = 1000000007;
+    public int maxProduct(TreeNode root) {
+        List<Long> sums = new ArrayList<>();
+        long total = helper(root, sums);
+        long max = 0;
+        for(long s : sums){
+            long p = s * (total - s);
+            max = Math.max(max, p);
+        }
+        return (int)(max % MOD);
+    }
+
+    public long helper(TreeNode node, List<Long> sums){
+        if(node == null){
+            return 0;
+        }
+        long left = helper(node.left, sums);
+        long right = helper(node.right, sums);
+        long sum = left + right + node.val;
+        sums.add(sum);
+        return sum;
+    }
+}
