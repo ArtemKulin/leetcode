d62d804e8deacc15a772c47da0c7203590da154a
Artem Kulin <kulinartem@gmail.com>
2022-12-13 17:47:50 +0300

    Add solution for minimum falling path sum

---
+++ tasks/src/minimum_falling_path_sum/Solution.java
@@ -0,0 +1,12 @@
+package minimum_falling_path_sum;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        for (int i = 1; i < matrix.length; ++i)
+            for (int j = 0; j < matrix.length; ++j)
+                matrix[i][j] += Math.min(matrix[i - 1][j], Math.min(matrix[i - 1][Math.max(0, j - 1)], matrix[i - 1][Math.min(matrix.length - 1, j + 1)]));
+        return Arrays.stream(matrix[matrix.length - 1]).min().getAsInt();
+    }
+}
