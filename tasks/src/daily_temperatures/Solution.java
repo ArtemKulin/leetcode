a1296c42a3c7433c313fd562519df60538c00662
Artem Kulin <kulinartem@gmail.com>
2022-12-18 10:07:38 +0300

    Add solution for daily temperatures

---
+++ tasks/src/daily_temperatures/Solution.java
@@ -0,0 +1,21 @@
+package daily_temperatures;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        for (int i = temperatures.length - 2; i >= 0; i--) {
+            int next = i + 1;
+            while (next < temperatures.length && temperatures[next] <= temperatures[i]) {
+                if (res[next] == 0) {
+                    next = temperatures.length;
+                    break;
+                }
+                next += res[next];
+            }
+            if (next < temperatures.length) res[i] = next - i;
+        }
+        return res;
+    }
+}
