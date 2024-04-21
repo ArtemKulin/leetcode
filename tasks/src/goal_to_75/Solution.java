4d489492b214021f4137dc60474fe9d7f314faaf
Artem Kulin <kulinartem@gmail.com>
2024-04-21 16:06:22 +0300

    Add solution for merge strings alternately

---
+++ tasks/src/goal_to_75/Solution.java
@@ -0,0 +1,25 @@
+package goal_to_75;
+
+public class Solution {
+    
+    public String mergeAlternately(String word1, String word2) {
+        var sb = new StringBuilder();
+        var length = Math.min(word1.length(), word2.length());
+        for (int i = 0; i < length; i++) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+        }
+        if (word1.length() > word2.length()) {
+            while (length < word1.length()) {
+                sb.append(word1.charAt(length));
+                length++;
+            }
+        } else {
+            while (length < word2.length()) {
+                sb.append(word2.charAt(length));
+                length++;
+            }
+        }
+        return sb.toString();
+    }
+}
