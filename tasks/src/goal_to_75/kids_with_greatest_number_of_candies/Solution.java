5f96d2e4852b0ab5656e0940a3d547017c92c562
Artem Kulin <kulinartem@gmail.com>
2024-05-30 13:44:51 +0300

    Add solution for kids with candies

---
+++ tasks/src/goal_to_75/kids_with_greatest_number_of_candies/Solution.java
@@ -0,0 +1,23 @@
+package goal_to_75.kids_with_greatest_number_of_candies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
